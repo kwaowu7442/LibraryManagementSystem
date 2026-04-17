2d95eafbb72470517e4c1ea26483ec096428ba1b
kwaowu7442 <kwaowu7442@students.ecpi.edu>
2026-04-16 20:52:16 -0400

    Add files via upload

--- Book.cs
+++ Book.cs
@@ -1,12 +1,10 @@
-/*
-Name: Kwadwo Owusu
-Date: 9th April 2026
-Purpose: Base class representing a book (Inheritance base class)
-*/
+/* Name: Kwadwo Owusu
+   Date: April 16, 2026 
+   Purpose: Base class updated to implement ILibraryItem */
 
 using System;
 
-public class Book
+public class Book : ILibraryItem // Now implements the interface
 {
     public int Id { get; set; }
     public string Title { get; set; }
@@ -19,9 +17,9 @@ public class Book
         Author = author;
     }
 
-    // Method to display book info
+    // This fulfills the interface requirement
     public virtual void Display()
     {
-        Console.WriteLine($"ID: {Id}, Title: {Title}, Author: {Author}");
+        Console.WriteLine($"[BOOK] ID: {Id}, Title: {Title}, Author: {Author}");
     }
 }
